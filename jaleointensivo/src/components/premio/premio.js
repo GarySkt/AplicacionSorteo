1ceaaf2dc66bcb2439e11e6f2e0ae40b49ef8a5d
LuisEdMB <maestro_edu_10@hotmail.com>
2020-04-05 21:45:39 -0500

    Se agrega scrollview a premio.

--- jaleointensivo/src/components/premio/premio.js
+++ jaleointensivo/src/components/premio/premio.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react"
-import { Container, Fab, Icon, Text } from "native-base"
+import React, { useEffect, useState } from "react"
+import { Container, Fab, Icon } from "native-base"
 
 import { createStackNavigator } from "react-navigation-stack"
 import { menu, menuStack } from "../../utils"
@@ -8,12 +8,18 @@ import FormularioPremioContainer from "./formulariopremio"
 import servicioPremio from "../../services/serviciosPremio/serviciosPremio"
 import * as actions from "../../actions/premio"
 import { connect } from "react-redux"
+import { ScrollView, RefreshControl } from "react-native"
 
 function Index(props){
-  
-  const ObtenerPremios = () =>{
+  const [refrescar, setRefrescar] = useState(false)
+  const RefrescarLista = () => {
+    setRefrescar(true)
+    ObtenerPremios()
+  }
+  const ObtenerPremios = () => {
     servicioPremio.obtenerPremios()
         .then(premios => {
+          setRefrescar(false)
           props.listarPremios(premios)
         })
   }
@@ -25,16 +31,20 @@ function Index(props){
   }, [])
     return(
         <Container>
+          <ScrollView refreshControl={
+            <RefreshControl refreshing={refrescar} onRefresh={() => RefrescarLista()}/>
+          }>
             <ListaPremio {... props.navigation}/>
-            <Fab
-              position="bottomRight"
-              style={{ backgroundColor: "#5067FF" }}
-              onPress={() => {
-                  props.setearPremioVacio()
-                  return props.navigation.navigate("FormularioPremio")
-                }}>
-              <Icon name="add" />
-            </Fab>
+          </ScrollView>
+          <Fab
+            position="bottomRight"
+            style={{ backgroundColor: "#5067FF" }}
+            onPress={() => {
+              props.setearPremioVacio()
+              return props.navigation.navigate("FormularioPremio")
+            }}>
+            <Icon name="add" />
+          </Fab>
         </Container>
     )
 }
